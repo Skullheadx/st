152d350c2e341ba7960da2117372af62426bfb24
Skullheadx <admonty1@protonmail.com>
2026-06-13 22:19:32 -0400

    switch to fira code nerd font

--- flake.nix
+++ flake.nix
@@ -5,39 +5,37 @@
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
 
-  outputs =
-    { self, nixpkgs }:
-    let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system; };
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {inherit system;};
+  in {
+    packages.${system}.default = pkgs.stdenv.mkDerivation {
+      pname = "st";
+      version = "0.9.3";
+      src = ./.;
 
-    in
-    {
-      packages.${system}.default = pkgs.stdenv.mkDerivation {
-        pname = "st";
-        version = "0.9.3";
-        src = ./.;
+      nativeBuildInputs = with pkgs; [
+        pkg-config
+        ncurses
+        fontconfig
+        freetype
+        pkgs.nerd-fonts.fira-code
+      ];
+      buildInputs = [
+        pkgs.libX11
+        pkgs.libxft
+        pkgs.nerd-fonts.fira-code
+      ];
 
-
-
-        nativeBuildInputs = with pkgs; [
-          pkg-config
-          ncurses
-          fontconfig
-          freetype
-        ];
-        buildInputs = [
-          pkgs.libX11
-          pkgs.libxft
-        ];
-
-        preInstall = ''
-          export TERMINFO=$out/share/terminfo
-          mkdir -p $TERMINFO $out/nix-support
-          echo "$terminfo" >> $out/nix-support/propagated-user-env-packages
-        '';
-        installFlags = [ "PREFIX=$(out)" ];
-
-      };
+      preInstall = ''
+        export TERMINFO=$out/share/terminfo
+        mkdir -p $TERMINFO $out/nix-support
+        echo "$terminfo" >> $out/nix-support/propagated-user-env-packages
+      '';
+      installFlags = ["PREFIX=$(out)"];
     };
+  };
 }
